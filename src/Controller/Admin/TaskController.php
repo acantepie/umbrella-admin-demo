edeb0a1255f9fa28de787a67ef7a5b48251f172c
Cantepie <adrien.cantepie@6tm.com>
2020-09-29 02:02:16 +0200

    Task component

--- src/Controller/Admin/TaskController.php
+++ src/Controller/Admin/TaskController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Validator\Constraints\Positive;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 /**
- * Class FileWriterController
+ * Class TaskController
  *
  * @Route("app-task")
  */
