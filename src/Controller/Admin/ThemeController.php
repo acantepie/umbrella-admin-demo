525ba426004e76ddc3e634aacfe0e73f5241cffe
Cantepie <adrien.cantepie@6tm.com>
2020-10-31 21:29:43 +0100

    add theme page

---
+++ src/Controller/Admin/ThemeController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller\Admin;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Umbrella\CoreBundle\Controller\BaseController;
+
+/**
+ * Class ThemeController
+ *
+ * @Route("/theme")
+ */
+class ThemeController extends BaseController
+{
+    /**
+     * @Route("")
+     */
+    public function indexAction()
+    {
+        return $this->render('theme/index.html.twig');
+    }
+}
