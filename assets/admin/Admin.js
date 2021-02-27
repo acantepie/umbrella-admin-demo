1252309f1e9f5184e84df5eb81a697ffa0e10be6
Cantepie <adrien.cantepie@6tm.com>
2021-02-27 02:07:19 +0100

    Update Umbrella

--- assets/admin/Admin.js
+++ assets/admin/Admin.js
@@ -1,8 +1,8 @@
 import './admin.scss';
-import 'umbrella_admin/Admin';
+import umbrellaApp from 'umbrella_admin/Admin';
 
-window.app.init();
-
-window.app.jsResponseHandler.registerAction('alert', function(params) {
+umbrellaApp.jsResponseHandler.registerAction('alert', function(params) {
     alert(params.msg);
 });
+
+umbrellaApp.init();
