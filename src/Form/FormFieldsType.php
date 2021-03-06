c850caf2b853910b0e1838e92d7e2264e7e126c1
acantepie <acantepie@users.noreply.github.com>
2021-03-06 00:10:53 +0000

    [php-cs-fixer] apply changes.

--- src/Form/FormFieldsType.php
+++ src/Form/FormFieldsType.php
@@ -10,7 +10,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Count;
 use Symfony\Component\Validator\Constraints\NotNull;
 use Umbrella\CoreBundle\Component\UmbrellaFile\Validator\Constraints\UmbrellaFileConstraint;
 use Umbrella\CoreBundle\Form\AsyncEntity2Type;
