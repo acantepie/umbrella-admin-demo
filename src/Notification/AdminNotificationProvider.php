f2a2f33395278569dd1415a93bd487b36b23a7d2
Cantepie <adrien.cantepie@6tm.com>
2021-07-24 13:45:33 +0200

    Up

--- src/Notification/AdminNotificationProvider.php
+++ src/Notification/AdminNotificationProvider.php
@@ -4,27 +4,32 @@ namespace App\Notification;
 
 use App\Entity\AdminNotification;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
 use Umbrella\AdminBundle\Notification\BaseNotificationProvider;
 
 class AdminNotificationProvider extends BaseNotificationProvider
 {
     private EntityManagerInterface $em;
+    private Security $security;
 
     /**
      * AdminNotificationProvider constructor.
      */
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, Security $security)
     {
         $this->em = $em;
+        $this->security = $security;
     }
 
-    public function findByUser($user): iterable
+    public function collect(): iterable
     {
         $qb = $this->em->createQueryBuilder();
         $qb->select('e');
         $qb->from(AdminNotification::class, 'e');
         $qb->orderBy('e.createdAt', 'DESC');
         $qb->setMaxResults(10);
+
+        $user = $this->security->getUser();
 
         if (null !== $user) {
             $qb->innerJoin('e.users', 'users');
